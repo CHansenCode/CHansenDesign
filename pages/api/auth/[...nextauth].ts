a2f2f1987344e19de4b48ea82aa9e5c8e3bd2fc1
CHansenCode <78411255+CHansenCode@users.noreply.github.com>
2023-01-09 19:03:27 +0100

    example cleanup

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -6,17 +6,9 @@ import clientPromise from "../../../lib/mongodb";
 import GoogleProvider from "next-auth/providers/google";
 
 async function checkEmail(mail: string) {
-  let split = mail.split("@");
+  const split = mail.split("@");
 
-  if (split[0] === "alberto" && split[1] === "veo.co") {
-    return true;
-  }
-
-  if (split[1] === process.env.ALLOWED_EMAIL) {
-    return true;
-  } else {
-    return false;
-  }
+  return true;
 }
 
 export const authOptions: NextAuthOptions = {
@@ -25,10 +17,6 @@ export const authOptions: NextAuthOptions = {
   adapter: MongoDBAdapter(clientPromise),
 
   providers: [
-    // GithubProvider({
-    //   clientId: process.env.GITHUB_ID,
-    //   clientSecret: process.env.GITHUB_SECRET,
-    // }),
     GoogleProvider({
       clientId: process.env.GOOGLE_ID,
       clientSecret: process.env.GOOGLE_SECRET,
